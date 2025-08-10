2666011cc1f38ba85557b9fa1427b40f7331c4f2
RAYEN <gayen274@gmail.com>
2025-08-10 23:52:47 +0100

    💡 Ajout/modification de fichiers pour [décrire ce que tu as fait]

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  keycloak: {
-    url: 'http://localhost:8082', // Update this to your production Keycloak URL
-    realm: 'Neo_TM',
-    clientId: 'Neo_TM_front',
-  }
-};
--- frontend/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  keycloak: {
-    url: 'http://localhost:8082',
-    realm: 'Neo_TM',
-    clientId: 'Neo_TM_front',
-  }
-};
